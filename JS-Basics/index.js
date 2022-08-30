dc0435c65c0116e78e24a728fd11ae3b64e507fa
unknown <Imran Aziz@MSI-A01334.mps.k12>
2022-08-30 12:57:40 -0700

    Java Script Project

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,2 +1,24 @@
 //this is my first java script code
 console.log("Hello World");
+console.log(name);
+
+/*
+Cannot be a reserved keyword
+Should be meaningful
+Cannot start with a number (1name)
+Cannot contain a space or hyphon (-)
+Are case-sensitive
+*/
+
+let firstName = "Imran";
+let lastName = "Aziz";
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = "Imran";
+let age = 15;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
