caa6bbc49173820af9b7f2d446a3fc849ea024a2
Ben <baziz10sa@gmail.com>
2022-09-22 13:09:29 -0700

    java script project

--- JS-Basics/game.js
+++ JS-Basics/game.js
@@ -1,6 +1,7 @@
 const actualNumber = Math.floor(Math.random() * 100);
 let userGuess = prompt("Can you guess the number?");
 let userAttempts = 1;
+let attempts = 0;
 while (userGuess != actualNumber) {
     if (userGuess < actualNumber) {
         userGuess = prompt('More');
@@ -14,16 +15,4 @@ if (userAttempts == 1) {
     alert('nice');
 } else {
     alert('cool');
-}
-
-let attempts = 5; 
-if (userGuess < actualNumber) {
-    attempts = attempts-1;
-} else if (userGuess > actualNumber) {
-    attempts = attempts-1;
-}
-
-if (attempts < 1) {
-    alert('Game Over');
-    document.location.reload();
 }
